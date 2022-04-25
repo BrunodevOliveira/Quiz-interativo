2219079ee5456e377008aeb46bd8179b3e9aee7d
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-04-25 11:23:53 -0300

    Refatoração da aplicação

--- app.js
+++ app.js
@@ -1,44 +1,22 @@
 const form = document.querySelector('.quiz-form')
 const inputs = document.querySelectorAll('input')
+const buttonContainer = form.querySelector('.text-center')
+
+const paragraphScore = document.createElement('p')
 
 const correctAnswers = ['B', 'B', 'B', 'B']
-const paragraphScore = document.createElement('p')
-paragraphScore.classList.add('my-5', 'text-white', 'bg-success','badge')
 
-const answerScore = (score) => {
+const getScoreMessage = score => ({
+  25: `😐 Sua pontuação foi de: ${score}! Vamos tentar novamente!`,
+  50: `🙂 Sua pontuação foi de: ${score}! Tente novamente, pode melhorar!`,
+  75: `😉 Sua pontuação foi de: ${score}! Parabéns!`,
+  100: `😁 Sua pontuação foi de: ${score}! Excelente! Está cada vez mais próximo da fluência em JavaScript!`   
+})[score] || `🤨 Sua pontuação foi de: ${score}! Refaça o ATER e tente novamente!`
 
-    const feedbackPhrases = {
-        score0: `🤨 Sua pontuação foi de: ${score}! Refaça o ATER e tente novamente!`,
-        score25: `😐 Sua pontuação foi de: ${score}! Vamos tentar novamente!`,
-        score50: `🙂 Sua pontuação foi de: ${score}! Tente novamente, pode melhorar!`,
-        socre75: `😉 Sua pontuação foi de: ${score}! Parabéns!`,
-        socre100: `😁 Sua pontuação foi de: ${score}! Excelente! Está cada vez mais próximo da fluência em JavaScript!`
-    }
-
-    switch (score) {
-        case 0:
-            paragraphScore.textContent = feedbackPhrases.score0
-            break
-
-        case 25:
-            paragraphScore.textContent = feedbackPhrases.score25
-            break
-
-        case 50:
-            paragraphScore.textContent = feedbackPhrases.score50
-            break
-
-        case 75:
-            paragraphScore.textContent = feedbackPhrases.socre75
-            break
-
-        case 100: 
-        paragraphScore.textContent = feedbackPhrases.socre100
-            break
-        
-    }
-
-    form.insertAdjacentElement('afterend', paragraphScore)
+const showScore = score => {
+    paragraphScore.textContent = getScoreMessage(score)
+    paragraphScore.classList.add('text-white', 'bg-success','badge')
+    buttonContainer.insertAdjacentElement('beforebegin', paragraphScore)
 }
 
 form.addEventListener('submit', event => {
@@ -46,19 +24,19 @@ form.addEventListener('submit', event => {
     
     let score = 0
 
-    const answers = [
+    const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
         form.inputQuestion3.value,
         form.inputQuestion4.value,
     ]
 
-    answers.forEach((answer, index) => {
+    userAnswers.forEach((userAnswer, index) => {
 
-        if(answer === correctAnswers[index]){
+        if(userAnswer === correctAnswers[index]){
             score += 25
         }
     }) 
     
-    answerScore(score)
+    showScore(score)
 })
